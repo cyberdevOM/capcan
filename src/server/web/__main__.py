d059ebd2bbbc35c83b47fb68227c6ed8594e7c18
cyberdevOM <oliver.maule@outlook.com>
2025-06-13 16:36:42 +0000

    Dev/Fix: continue website fixed bugs

--- src/server/web/__main__.py
+++ src/server/web/__main__.py
@@ -1,4 +1,5 @@
-from web import app
+from .routes import app
+from .app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/server/web/app.py
+++ src/server/web/app.py
@@ -20,5 +20,5 @@ def Clients():
 def Settings():
     return render_template('Capcan-html-settings.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+    # app.run(debug=True)
