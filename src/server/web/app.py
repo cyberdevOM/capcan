0ca73651dc58c3550608eb99f83d15c9c9034946
cyberdevOM <oliver.maule@outlook.com>
2025-06-15 21:35:13 +0000

    Dev/Fix: dashboard good, current working project darkmode

--- src/server/web/app.py
+++ src/server/web/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, url_for 
-from templates.pre_renders.dashboard_prerender import export_client_list, render_quickstats
+import os, importlib.util
+
+file_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), './templates/pre_renders/dashboard_prerender.py')
+)
+
+spec = importlib.util.spec_from_file_location("dashboard_prerender", file_path)
+dashboard_preprender = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(dashboard_preprender)
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def Dashboard():
-    quickstats_html = render_quickstats()
+    quickstats_html = dashboard_preprender.render_quickstats()
     return render_template(
         'Capcan-html-home.html',
         quickstats_html=quickstats_html,
@@ -15,7 +23,7 @@ def Dashboard():
 def Clients():
     return render_template(
         'Capcan-html-clients.html', 
-        clients=export_client_list()
+        clients=dashboard_preprender.export_client_list()
     )
 
 @app.route('/settings')
